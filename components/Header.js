a3be0a640fe58f4db1448634d25dce4ba0476bbd
Rajcody <raj.laskar2016@gmail.com>
2021-04-29 12:42:10 +0530

    made search input visible

--- components/Header.js
+++ components/Header.js
@@ -36,7 +36,9 @@ function Header() {
                 
                 />
                 <form className='flex  flex-grow px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center'>
-                    <input ref={searchInputRef}  type='text' className='flex-grow w-4 focus:outline-none' />
+                    <input ref={searchInputRef} 
+                        defaultValue={router.query.term}
+                        type='text' className='flex-grow w-4 focus:outline-none' />
                     <XIcon className='h-7 sm:mr-3
                     text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125
                     
